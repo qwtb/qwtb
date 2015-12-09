feca610490dc6a7e280d2678707380d632f217aa
martin <martin@6011msira-lin>
2015-12-09 12:08:19 +0100

    add: algorithms for allan variation. could contain bugs

---
+++ qwtb/alg_MADEV/alg_example.m
@@ -0,0 +1,35 @@
+%% Modified Allan Deviation
+% Example for algorithm MADEV.
+%
+% MADEV is an algorithm to compute the modified Allan deviation for a set of time-domain frequency data.
+%
+% See also W. J. Riley, "The Calculation of Time Domain Frequency Stability". Implementation: M. A. Hopcroft, mhopeng@gmail.com, Matlab Central.'
+
+%% Generate sample data
+DI = [];
+%!demo
+%ysin=2.*sin(2.*pi.*1/300.*[1:1:1e3]);
+%! [x y s errors]=madev(1, ysin, 1, 'best averaging time is 300 s, i.e. cca one sine period');
+% A random numbers with normal probability distribution function will be geneated into data input |DI.y.v|.
+DI.y.v = normrnd(1.5,3,1,1e3);
+% Next a drift is added:
+DI.y.v = DI.y.v + [1:1:1e3]./100;
+% Lets suppose a sampling frequency is 1 Hz:
+DI.fs.v = 1;
+% Let the algorithm generate all possible tau values automatically:
+DI.tau.v = [];
+
+%% Call algorithm
+% Use QWTB to apply algorithm |MADEV| to data |DI|.
+DO = qwtb('MADEV', DI);
+
+%% Display results
+% Log log figure is the best to see modified allan deviation results:
+figure; hold on
+loglog(DO.tau.v, DO.madev.v, '-b')
+loglog(DO.tau.v, DO.madev.v + DO.madev.u, '-k')
+loglog(DO.tau.v, DO.madev.v - DO.madev.u, '-k')
+xlabel('\tau (sec)');
+ylabel('\sigma_y(\tau)');
+title(['period = ' num2str(DI.fs.v)]);
+grid('on'); hold off
---
+++ qwtb/alg_MADEV/alg_info.m
@@ -0,0 +1,19 @@
+function alginfo = alg_info() %<<<1
+% Part of QWTB. Info script for algorithm MADEV.
+%
+% See also qwtb
+
+alginfo.id = 'MADEV';
+alginfo.name = 'Modified Allan Deviation';
+alginfo.desc = 'Compute the modified Allan deviation for a set of time-domain frequency data.';
+alginfo.citation = 'W. J. Riley, "The Calculation of Time Domain Frequency Stability". Implementation: M. A. Hopcroft, mhopeng@gmail.com, Matlab Central.';
+alginfo.remarks = 'If tau is empty array, tau values are automatically generated. Tau values must be divisible by 1/fs. Invalid values are ignored. For tau values really used in the calculation see the output. Using sigma as uncertainty is probably not correct.';
+alginfo.license = 'BSD License';
+alginfo.requires = {'y', 'fs', 'tau'};
+alginfo.reqdesc = {'sampled values', 'sampling frequency', 'observation time'};
+alginfo.returns = {'madev', 'tau'};
+alginfo.retdesc = {'modified Allan deviation', 'observation time of result values'};
+alginfo.providesGUF = 1;
+alginfo.providesMCM = 0;
+
+% vim settings modeline: vim: foldmarker=%<<<,%>>> fdm=marker fen ft=octave textwidth=80 tabstop=4 shiftwidth=4
