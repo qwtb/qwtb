357530156401f3e2ba77356f38e528c65b2ea119
KaeroDot <KaeroDot@contact.me.on.git.hub>
2016-02-21 23:15:03 +0100

    add: documentation of algorithms updated to a new alg_info files format, better look of algorithms info documentation, recompiled algorithms documentation, imrpoved local function in qwtb.m without affect on qwtb run, copy of this local function is in doc/publishscripts/parse_alginfo_inputs.m

--- doc/publishscripts/all_algs_info2tex.m
+++ doc/publishscripts/all_algs_info2tex.m
@@ -13,16 +13,15 @@ addpath('../../qwtb');
 % get all informations from qwtb:
 infos = qwtb();
 
-for i = 1:length(infos)
-    % for all info:
+for i = 1:length(infos) % for all info: %<<<1
     disp(['algorithm ' infos(i).id]);
     fid = fopen([dirprefix 'info_' infos(i).id '.tex'], 'w');
     % add proper header:
     fprintf(fid, '\\begin{tightdesc}\n');
     % add basic informations:
-    fprintf(fid, '\\item [\\textsf{.id}] --- %s\n', infos(i).id);
-    fprintf(fid, '\\item [\\textsf{.name}] --- %s\n', infos(i).name);
-    fprintf(fid, '\\item [\\textsf{.desc}] --- %s\n', infos(i).desc);
+    fprintf(fid, '\\item [Id:] %s\n', infos(i).id);
+    fprintf(fid, '\\item [Name:] %s\n', infos(i).name);
+    fprintf(fid, '\\item [Description:] %s\n', infos(i).desc);
     tmp = infos(i).citation;
     % match urls in citation field and format urls:
     [S, E, TE, M, T, NM, SP] = regexp (tmp, 'https?://\S+');
@@ -34,38 +33,112 @@ for i = 1:length(infos)
         tmp = [tmp SP{j+1}];
     end
     % add other informations
-    fprintf(fid, '\\item [\\textsf{.citation}] --- %s\n', tmp);
-    fprintf(fid, '\\item [\\textsf{.remarks}] --- %s\n', infos(i).remarks);
-    fprintf(fid, '\\item [\\textsf{.license}] --- %s\n', infos(i).license);
-    % add description field for all required quantities:
-    fprintf(fid, '\\item [\\textsf{.requires}] \\rule{0em}{0em}\n');
-        fprintf(fid, '\\begin{tightdesc}\n');
-        for j = 1:length(infos(i).requires)
-            fprintf(fid, '\\item [\\textsf{%s}] --- %s\n', infos(i).requires{j}, infos(i).reqdesc{j});
-        end
-        fprintf(fid, '\\end{tightdesc}\n');
-    % add description field for all returned quantities:
-    fprintf(fid, '\\item [\\textsf{.returns}] \\rule{0em}{0em}\n');
-        fprintf(fid, '\\begin{tightdesc}\n');
-        for j = 1:length(infos(i).returns)
-            fprintf(fid, '\\item [\\textsf{%s}] --- %s\n', infos(i).returns{j}, infos(i).retdesc{j});
-        end
-        fprintf(fid, '\\end{tightdesc}\n');
+    fprintf(fid, '\\item [Citation:] %s\n', tmp);
+    fprintf(fid, '\\item [Remarks:] %s\n', infos(i).remarks);
+    fprintf(fid, '\\item [License:] %s\n', infos(i).license);
     % add informations about uncertainty calculation:
     if infos(i).providesGUF
         tmp = 'yes';
     else
         tmp = 'no';
     end
-    fprintf(fid, '\\item [\\textsf{.providesGUF}] --- %s\n', tmp);
+    fprintf(fid, '\\item [Provides GUF:] %s\n', tmp);
     if infos(i).providesMCM
         tmp = 'yes';
     else
         tmp = 'no';
     end
-    fprintf(fid, '\\item [\\textsf{.providesMCM}] ---  %s\n', tmp);
+    fprintf(fid, '\\item [Provides MCM:] %s\n', tmp);
+    % add list and descriptions of all input quantities: %<<<2
+    % XXX this function does not differ optional/nonoptional parameters?
+    [reqQ, reqQdesc, optQ, optQdesc, reqQG, reqQGdesc, optQG, optQGdesc, parQ, Qlist] = parse_alginfo_inputs(infos(i));
+    % input quantites:
+    fprintf(fid, '\\item [Input Quantities] \\rule{0em}{0em}\n    \\begin{tightdesc}');
+        % required quantities: %<<<3
+        tmp = '';
+        % prepare all required grouped quantities:
+        for j = 1:length(reqQG)
+            if j > 1
+                tmp = [tmp ',\enspace '];
+            end
+            for k = 1:length(reqQG{j})
+                if k > 1
+                    tmp = [tmp ' or '];
+                end
+                tmp = [tmp '\textsf{' reqQG{j}{k} '}'];
+            end
+        end
+        % prepare all required quantities:
+        for j = 1:length(reqQ)
+            if (j == 1 && isempty(tmp))
+                tmp = [tmp '\textsf{' reqQ{j} '}'];
+            else
+                tmp = [tmp ',\enspace \textsf{' reqQ{j} '}'];
+            end
+        end
+        % write prepared to the file:
+        if ~isempty(tmp)
+            fprintf(fid, '\n    \\item [Required:] \n        ');
+            fprintf(fid, '%s', tmp);
+        end
+        % optional quantities: %<<<3
+        tmp = [];
+        % prepare all optional grouped quantities
+        for j = 1:length(optQG)
+            for k = 1:length(optQG{j})
+                if k == 1
+                    tmp = [tmp '\textsf{' optQG{j}{k} '}'];
+                else
+                    tmp = [tmp ' or \textsf{' optQG{j}{k} '}'];
+                end
+            end
+        end
+        % write all optional quantities
+        for j = 1:length(optQ)
+            if (j == 1 && isempty(tmp))
+                tmp = [tmp '\textsf{' optQ{j} '}'];
+            else
+                tmp = [tmp ',\enspace \textsf{' optQ{j} '}'];
+            end
+        end
+        % write prepared to the file:
+        if ~isempty(tmp)
+            fprintf(fid, '\n    \\item [Optional:] \n        ');
+            fprintf(fid, '%s', tmp);
+        end
+        % parameter quantities: %<<<3
+        tmp = [];
+        % prepare all parameters:
+        for j = 1:length(parQ)
+            if j > 1
+                tmp = [tmp ',\enspace '];
+            end
+            tmp = [tmp '\textsf{' parQ{j} '}'];
+        end
+        % write prepared to the file:
+        if ~isempty(tmp)
+            fprintf(fid, '\n    \\item [Parameters:] \n        ');
+            fprintf(fid, '%s', tmp);
+        end
+        % input quantities descriptions: %<<<3
+        fprintf(fid, '\n    \\end{tightdesc}\n\\item [Descriptions:] \\rule{0em}{0em}\n    \\begin{tightdesc}');
+        for j = 1:size(Qlist, 1)
+            fprintf(fid, '\n        \\item[\\textsf{%s}] -- %s', Qlist{j,1}, Qlist{j,2});
+        end
+    fprintf(fid, '\n    \\end{tightdesc}\n');
+    % add list and descriptions of all output quantities: %<<<2
+    fprintf(fid, '\\item [Output Quantities] \\rule{0em}{0em}\n    \\begin{tightdesc}');
+    % make a list of output quantities:
+    Qlist = [{infos(i).outputs.name}; {infos(i).outputs.desc}]';
+    [tmp, ind] = sort(Qlist(:,1));
+    ind = ind(:,1);
+    Qlist = {Qlist{ind,1}; Qlist{ind,2}}';
+    for j = 1:size(Qlist,1);
+        fprintf(fid, '\n        \\item[\\textsf{%s}] -- %s', Qlist{j,1}, Qlist{j,2});
+    end
+    fprintf(fid, '\n    \\end{tightdesc}\n');
     fprintf(fid, '\\end{tightdesc}\n');
-    % close the file
+    % close the file: %<<<2
     fclose(fid);
 end % for all info:
 
--- qwtb/qwtb.m
+++ qwtb/qwtb.m
@@ -637,6 +637,92 @@ function calcset = check_gen_calcset(calcset) %<<<1
     end
 end % function check_calcset
 
+function [reqQ, reqQdesc, optQ, optQdesc, reqQG, reqQGdesc, optQG, optQGdesc, parQ, Qlist] = parse_alginfo_inputs(alginfo) %<<<1
+% parse requirements of the algorithm as stated in algorithm info structure
+% returns:
+% reqQ - cell of strings of names of required quantities
+% reqQdesc - cell of strings of descriptions of reqQ
+% optQ - cell of strings of names of optional quantities
+% optQdesc - cell of strings of descriptions of optQ
+% reqQG - cell of cell of strings of names of grouped quantities (one array
+%       element is one group) in which at least one quantity is required, therefore
+%       whole group is required
+% reqQGdesc - cell of cell of strings of descriptions of reqQG
+% optQG - cell of cell of strings of names of grouped quantities (one array
+%       element is one group) in which all quantities are optional, therefore whole
+%       group is optional
+% optQGdesc - cell of cell of strings of descriptions of optQG
+% parQ - cell of strings of names of parameter quantities (parameters)
+% Qlist - cell with {x,1} names and {x,2} descriptions of all input quantities of algorithm, sorted by names of quantities
+
+    reqQ = {};
+    reqQdesc = {};
+    optQ = {};
+    optQdesc = {};
+    tmpQG = {};
+    tmpQGdesc = {};
+    tmpQGopt = {};
+    parQ = {};
+    Qlist = {};
+
+    for i = 1:length(alginfo.inputs)
+        % for every input quantity
+        Q = alginfo.inputs(i);
+        % prepare list of Quantities:
+        Qlist{i, 1} = Q.name;
+        Qlist{i, 2} = Q.desc;
+        if Q.alternative > 0
+            % grouped quantity -> store name, description and optionality:
+            tmpQG{Q.alternative}{end+1} = Q.name;
+            tmpQGdesc{Q.alternative}{end+1} = Q.desc;
+            tmpQGopt{Q.alternative}{end+1} = Q.optional;
+        else
+            % not-grouped quantity:
+            if Q.optional
+                % optional not-grouped quantity -> store name and description:
+                optQ{end+1} = Q.name;
+                optQdesc{end+1} = Q.desc;
+            else
+                % not-optional not-grouped quantity -> store name and
+                % description:
+                reqQ{end+1} = Q.name;
+                reqQdesc{end+1} = Q.desc;
+            end
+        end % if Q.alternative > 0
+        % check for parameter settings
+        if Q.parameter
+            parQ{end+1} = Q.name;
+        end
+    end % for every required quantity
+
+    % check that no empty elements are in reqQG (Q.alternative is incremental by
+    % one and e.g. 2, 5, 10). separate required and optional groups.
+    reqQG = {};
+    reqQGdesc = {};
+    optQG = {};
+    optQGdesc = {};
+    for i = 1:length(tmpQG)
+        if ~isempty(tmpQG{i}) % some groups numbers maybe do not exist
+            % check optionality of group. if all required quantities in this
+            % group are NOT optional -> group is NOT optional (and one of
+            % quantities in group must be in input data)
+            if all([tmpQGopt{i}{:}])
+                optQG{end+1} = tmpQG{i};
+                optQGdesc{end+1} = tmpQGdesc{i};
+            else
+                reqQG{end+1} = tmpQG{i};
+                reqQGdesc{end+1} = tmpQGdesc{i};
+            end % if all
+        end % if isempty
+    end % for i
+
+    % sort list of Quantities by name:
+    [tmp, ind] = sort(Qlist(:,1));
+    ind = ind(:,1);
+    Qlist = {Qlist{ind,1}; Qlist{ind,2}}';
+
+end % function parse_alginfo_inputs(alginfo)
+
 function datain = check_gen_datain(alginfo, datain, calcset) %<<<1
 % Checks if input data has all quantities required by algorithm.
 % Checks if input data complies to the qwtb data format.
@@ -645,7 +731,7 @@ function datain = check_gen_datain(alginfo, datain, calcset) %<<<1
 
     % search for missing quantities in input data structure: %<<<2
     % parse requirements of the algorithm:
-    [reqQ, reqQdesc, optQ, optQdesc, reqQG, reqQGdesc, optQG, optQGdesc, parQ] = parse_alginfo_inputs(alginfo);
+    [reqQ, reqQdesc, optQ, optQdesc, reqQG, reqQGdesc, optQG, optQGdesc, parQ, Qlist] = parse_alginfo_inputs(alginfo);
     % compare requirements with datain:
     errmsg = check_Q_present(datain, alginfo, reqQ, reqQdesc, optQ, optQdesc, reqQG, reqQGdesc, optQG, optQGdesc);
     if ~isempty(errmsg)
@@ -897,81 +983,6 @@ function datain = check_gen_datain(alginfo, datain, calcset) %<<<1
     end % for every Q
 end % function check_gen_datain
 
-function [reqQ, reqQdesc, optQ, optQdesc, reqQG, reqQGdesc, optQG, optQGdesc, parQ] = parse_alginfo_inputs(alginfo) %<<<1
-% parse requirements of the algorithm as stated in algorithm info structure
-% returns:
-% reqQ - cell of strings of names of required quantities
-% reqQdesc - cell of strings of descriptions of reqQ
-% optQ - cell of strings of names of optional quantities
-% optQdesc - cell of strings of descriptions of optQ
-% reqQG - cell of cell of strings of names of grouped quantities (one array
-%       element is one group) in which at least one quantity is required, therefore
-%       whole group is required
-% reqQGdesc - cell of cell of strings of descriptions of reqQG
-% optQG - cell of cell of strings of names of grouped quantities (one array
-%       element is one group) in which all quantities are optional, therefore whole
-%       group is optional
-% optQGdesc - cell of cell of strings of descriptions of optQG
-% parQ - cell of strings of names of parameter quantities (parameters)
-
-    reqQ = {};
-    reqQdesc = {};
-    optQ = {};
-    optQdesc = {};
-    tmpQG = {};
-    tmpQGdesc = {};
-    tmpQGopt = {};
-    parQ = {};
-
-    for i = 1:length(alginfo.inputs)
-        % for every input quantity
-        Q = alginfo.inputs(i);
-        if Q.alternative > 0
-            % grouped quantity -> store name, description and optionality:
-            tmpQG{Q.alternative}{end+1} = Q.name;
-            tmpQGdesc{Q.alternative}{end+1} = Q.desc;
-            tmpQGopt{Q.alternative}{end+1} = Q.optional;
-        else
-            % not-grouped quantity:
-            if Q.optional
-                % optional not-grouped quantity -> store name and description:
-                optQ{end+1} = Q.name;
-                optQdesc{end+1} = Q.desc;
-            else
-                % not-optional not-grouped quantity -> store name and
-                % description:
-                reqQ{end+1} = Q.name;
-                reqQdesc{end+1} = Q.desc;
-            end
-        end % if Q.alternative > 0
-        % check for parameter settings
-        if Q.parameter
-            parQ{end+1} = Q.name;
-        end
-    end % for every required quantity
-
-    % check that no empty elements are in reqQG (Q.alternative is incremental by
-    % one and e.g. 2, 5, 10). separate required and optional groups.
-    reqQG = {};
-    reqQGdesc = {};
-    optQG = {};
-    optQGdesc = {};
-    for i = 1:length(tmpQG)
-        if ~isempty(tmpQG{i}) % some groups numbers maybe do not exist
-            % check optionality of group. if all required quantities in this
-            % group are NOT optional -> group is NOT optional (and one of
-            % quantities in group must be in input data)
-            if all([tmpQGopt{i}{:}])
-                optQG{end+1} = tmpQG{i};
-                optQGdesc{end+1} = tmpQGdesc{i};
-            else
-                reqQG{end+1} = tmpQG{i};
-                reqQGdesc{end+1} = tmpQGdesc{i};
-            end % if all
-        end % if isempty
-    end % for i
-
-end % function parse_alginfo_inputs(alginfo)
 
 function errmsg = check_Q_present(datain, alginfo, reqQ, reqQdesc, optQ, optQdesc, reqQG, reqQGdesc, reqQGopt) %<<<1
 % checks if quantities present in datain meet requirements according required,
