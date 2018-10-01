463ab58ea58b2b4a6834bf5e9b27dbce1f95169f
KaeroDot <KaeroDot@contact.me.on.git.hub>
2018-10-01 13:44:07 +0200

    add: version of ThreePSF from Lapuh's book

--- qwtb/alg_ThreePSF/ThreePSF.m
+++ qwtb/alg_ThreePSF/ThreePSF.m
@@ -1,34 +1,31 @@
-function [A phi O] = ThreePSF(Record,f,Ts)
-% 3PSF (Three Parameter Sine Fit)
-% calculates amplitude, phase and offset
-%
-%       Input arguments
-%       Record     - sampled input signal
-%       f          - signal frequency
-%       Ts         - sampling time (in s)
-%       StartIndex - starting sample
-%       Length     - number of samples used
-%       harmonic   - harmonic component to be calculated
-%
-%       Output arguments
-%       A      - estimated signal amplitude
-%       phi    - estimated signal phase
-%       O      - estimated signal offset
+ function [A phi O] = ThreePSF(Record,f,Ts)
+ % Three Parameter Sine Fit (3PSF)
+ %
+ %  [A phi O] = ThreePSF(Record,f,Ts)
+ %  Input arguments
+ %    Record     - sampled input signal
+ %    f          - signal frequency
+ %    Ts         - sampling time (in s)
+ %  Output arguments
+ %    A          - estimated signal's amplitude
+ %    phi        - estimated signal's phase
+ %    O          - estimated signal's offset
+ %
+ % Reference: IEEE Standard for Digitizing Waveform Recorders 
+ % IEEE Instrumentation and Measurement Society,  
+ % IEEE Std 1057TM-2007 (Revision of IEEE 1057-1994)
+ % (c) Rado Lapuh, 2016
 
-% (c) Rado Lapuh
+ N = length(Record);             % determine the number of samples
+ Record = Record(:);             % force column vector
 
-M = length(Record);
-Record = Record(:); %Force column vector
-
-fTs = 2*pi*f*Ts;
-vct = fTs*(0:M-1);
-D0 = [cos(vct);sin(vct);ones(1,M)]';
-x1 = D0\Record;
-
-phi =atan(-x1(2)/x1(1))-pi/2;
-if x1(1) >= 0 
-    phi = phi + pi; 
-end
-A = sqrt(x1(1)^2+x1(2)^2);
-O = x1(3);
-
+ vct = 2*pi*f*Ts*(0:N-1);
+ D0 = [cos(vct);sin(vct);ones(1,N)]'; % (Eq. A.3)
+ x1 = D0\Record;                 % solve system of linear equations 
+                                 % (Eq. A.7)
+ phi =atan(-x1(2)/x1(1))-pi/2;   % phase estimation (Eq. A.10)
+ if x1(1) >= 0                   % wrap phase within <-pi,pi>
+   phi = phi + pi; 
+ end
+ A = sqrt(x1(1)^2+x1(2)^2);      % amplitude estimation (Eq. A.9)
+ O = x1(3);                      % offset estimation
